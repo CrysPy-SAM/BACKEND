48dc04c1666f2f3aae05f7a6d94c09479c03a850
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-18 00:20:04 +0530

    changes.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 
-        "User registered successfully"
-    });
+   //get user detail from frontend
+   //validate user data - not empty
+   //check if user already exists   
+   //check for images, check for avatar
+   //upload them to cloudinary, avtar
+   //create user obj - create user in db
+   // remove password and refresh token feild from response
+   //check user for creation
+   //return response 
+
+   const {fullName, email, username, password}= req.body
+    console.log("email", email);
+    
 });
 
 export { registerUser };
