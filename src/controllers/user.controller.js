ee6331f1e6418c300bc8551d7a9f92d3b45dd2f1
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-18 10:33:24 +0530

    add register controller and bug fixes.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponce.js";
+
+
 const registerUser = asyncHandler(async (req, res) => {
    //get user detail from frontend
    //validate user data - not empty
@@ -12,6 +18,53 @@ const registerUser = asyncHandler(async (req, res) => {
 
    const {fullName, email, username, password}= req.body
     console.log("email", email);
+
+   if(!fullName || !email || !username || !password) {
+       throw new ApiError(400, "Please fill all the fields");   
+    }    
+    
+    const existedUser = await User.find ({
+         $or: [{username}, {email}] 
+    })
+    if(existedUser) {
+        throw new ApiError(409, "User already exists with this username or email");
+    }
+
+
+    const avatarLocalPath = req.files.avatar[0]?.path;
+    const coverImageLocalPath = req.files.coverImage[0]?.path;
+   
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Please upload an avatar image");
+    } 
+
+    const avtar = await uploadOnCloudinary(avatarLocalPath, "avatars");
+    const coverImage = await uploadOnCloudinary (coverImageLocalPath, "coverImages");
+     
+    if (!avtar || !coverImage) {
+        throw new ApiError(500, "Error uploading images to cloudinary");
+    } 
+
+   const user = await User.create({
+        fullName,   
+        avatar: avtar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        username: username.toLowerCase(),
+        password, // password will be hashed in the User model pre-save hook
+    })
+
+    const createdUser = await User.findById(user._id).select
+    ("-password -refreshToken");
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong, user not created");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, createdUser, "User registered successfully")
+    );
     
 });
 
